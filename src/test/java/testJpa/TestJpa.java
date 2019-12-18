557e2a784d7aa8e59c506efa56b6162f10a3f0eb
ccandrei <craciunescu.andrei@gmail.com>
2019-12-18 15:48:38 +0100

    TP 2

--- src/test/java/testJpa/TestJpa.java
+++ src/test/java/testJpa/TestJpa.java
@@ -1,14 +1,18 @@
+package testJpa;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import tp.Jpa.*;
 
 public class TestJpa {
 	
 	public static void main (String[] args) {
 		
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bibliothéque");
 		EntityManager em =entityManagerFactory.createEntityManager();
+		
+		Livre li = em.find(Livre.class, 2);
+		System.out.println(li.getTitre());
 		
 		em.close();
 		entityManagerFactory.close();
